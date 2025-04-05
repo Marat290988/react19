9f85b07d19c46c1361e38e898cff4f828155d646
marat3 <marat@appsolutemedia.com>
2025-04-05 18:48:44 +0500

    next commit

--- src/pages/orders/new-order/new-order.tsx
+++ src/pages/orders/new-order/new-order.tsx
@@ -63,6 +63,7 @@ export const NewOrder: React.FC = () => {
           setOrder(order);
           setLocalLoading(false);
         } else {
+          setLocalLoading(false);
           navigate('/' + Path.ORDERS + '/' + PathOrder.NEW);
         }
       })
